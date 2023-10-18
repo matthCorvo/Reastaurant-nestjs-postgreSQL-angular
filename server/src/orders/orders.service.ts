4e5b4d938963838cca5a722b3eba50fdafa6c871
matthieu Corvo <76264628+matthCorvo@users.noreply.github.com>
2023-10-18 03:03:39 +0200

    Update orders.service.ts

--- server/src/orders/orders.service.ts
+++ server/src/orders/orders.service.ts
@@ -106,7 +106,7 @@ export class OrdersService {
    * Supprime lla commande payée pour un utilisateur donné avec un statut "NEW".
    * @param id - Identifiant de la commande
    */
-    async delete(id: number): Promise<void> {
+    async delete(id: number){
      return this.orderRepository.delete( id );
     }
   
